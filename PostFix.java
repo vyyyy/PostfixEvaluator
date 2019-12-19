659e9253d3191e9fa79169537c22ebc79740d981
vyyyy <vpat5777@uni.sydney.edu.au>
2019-12-20 00:09:50 +1100

    Refactored postfix evaluator and implemented simple cell referencing

--- PostFix.java
+++ PostFix.java
@@ -1,57 +1,87 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.util.Scanner;
 import java.util.Stack;
-import java.lang.Math;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.lang.Math;
 
+/**
+ * The Postfix program evaluates postfix expressions in a given CSV file and
+ * outputs the results on the screen. Additionally, it saves a CSV file
+ * containing the evaluated postfix expressions in the same dimensions of the
+ * input CSV file.
+ * 
+ * @author Vyoma Patel
+ * @version 1.0.1
+ * @since 2019-12-19
+ */
 public class PostFix {
     public static void main(String[] args) {
+
         // Input file which needs to be parsed
         String fileToParse = "SampleCSVFile.csv";
         if (args.length > 0) {
             fileToParse = args[0];
         }
-        BufferedReader fileReader = null;
 
         // Delimiter used in CSV file
         final String DELIMITER = ",";
+
+        // LinkedHashmap to store cell references similar to a spreadsheet
+        LinkedHashMap<String, String> mapping = new LinkedHashMap<String, String>();
 
         // List to collect postfix expressions, which is saved to output csv file
         List<String> collectTokens = new ArrayList<String>();
 
         try {
-            String line = "";
-            // Create the file reader
-            fileReader = new BufferedReader(new FileReader(fileToParse));
+            // Load the file for reading and parsing
+            Scanner fileReader = new Scanner(new File(fileToParse));
+
+            // Initialise rows in a csv file
+            int Rows = 1;
 
             // Read the file line by line
-            while ((line = fileReader.readLine()) != null) {
+            while (fileReader.hasNext()) {
                 // Get all tokens available in line
-                String[] tokens = line.split(DELIMITER);
+                String[] tokens = fileReader.nextLine().split(DELIMITER);
+
+                // Initialise columns in a csv file
+                int Cols = 1;
 
                 for (String token : tokens) {
                     // Add token to scanner
                     Scanner tokenScanner = new Scanner(token);
-                        // Run postfix evaluation on token if it's valid 
-                        if (tokenScanner.hasNextDouble()) {
-                            double result = evaluatePostfix(token);
-                            // Print to STDOUT
-                            System.out.println(result);
-                            // Add postfix evaluation of token to a list
-                            collectTokens.add(String.valueOf(result));
-                        } else {
-                            // The token cannot be evaluated as a postfix expression
-                            collectTokens.add("#ERR");
-                        }
+                    // Run postfix evaluation on token if it's valid
+                    if (tokenScanner.hasNextInt()) {
+                        int result = evaluatePostfix(token);
+                        // Add postfix evaluation of token to a list
+                        collectTokens.add(String.valueOf((int) result));
+                        // Add cell reference and token to linkedhashmap
+                        mapping.put(columnToLetter(Cols) + String.valueOf(Rows), String.valueOf(result));
+                        // Print to STDOUT
+                        System.out.println(result);
+                    } else {
+                        // The token cannot be evaluated as a postfix expression
+                        collectTokens.add("#ERR");
+                        // Add cell reference and token to linkedhashmap
+                        mapping.put(columnToLetter(Cols) + String.valueOf(Rows), "#ERR");
+                        // Print to STDOUT
+                        System.out.println("#ERR");
+                    }
                     tokenScanner.close();
+                    // Keep track of columns
+                    Cols++;
                 }
                 // Add a newline after each line is read for output csv file
                 collectTokens.add(System.lineSeparator());
+                // Keep track of rows
+                Rows++;
             }
+            // Close the file
             fileReader.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -63,21 +93,27 @@ public class PostFix {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // @TODO: remove STDOUT
+        System.out.println(mapping);
     }
 
-    // Evaluate postfix expression
-    static double evaluatePostfix(String expression) {
+    /**
+     * Evaluate postfix expression
+     * 
+     * @param expression The postfix expression to evaluate in a given cell
+     * @return The evaluated postfix expression of a given cell
+     */
+    public static int evaluatePostfix(String expression) {
 
         Scanner scanner = new Scanner(expression);
-        Stack<Double> stack = new Stack<Double>();
+        Stack<Integer> stack = new Stack<Integer>();
 
         while (scanner.hasNext()) {
-
-            if (scanner.hasNextDouble()) {
-                stack.push(scanner.nextDouble());
+            if (scanner.hasNextInt()) {
+                stack.push(scanner.nextInt());
             } else {
-                double b = stack.pop();
-                double a = stack.pop();
+                int b = stack.pop();
+                int a = stack.pop();
                 char operator = scanner.next().charAt(0);
                 switch (operator) {
                 case '+':
@@ -101,7 +137,7 @@ public class PostFix {
                     break;
 
                 case '^':
-                    stack.push(Math.pow(a, b));
+                    stack.push((int) Math.pow(a, b));
                     break;
                 }
             }
@@ -110,12 +146,43 @@ public class PostFix {
         return stack.pop();
     }
 
-    // Output csv file
+    /**
+     * Convert a column in a CSV file into Excel column
+     * 
+     * @param columnNumber The column's position in a CSV file
+     * @return The letter corresponding to the column position
+     */
+    public static String columnToLetter(int columnNumber) {
+        // Store result
+        StringBuilder columnName = new StringBuilder();
+
+        while (columnNumber > 0) {
+            // Find remainder
+            int remainder = columnNumber % 26;
+            // If remainder is 0
+            if (remainder == 0) {
+                columnName.append("Z");
+                columnNumber = (columnNumber / 26) - 1;
+            } else {
+                // If remainder is non-zero
+                columnName.append((char) ((remainder - 1) + 'A'));
+                columnNumber = columnNumber / 26;
+            }
+        }
+        // Convert to string, make it lowercase and reverse it
+        return columnName.reverse().toString().toLowerCase();
+    }
+
+    /**
+     * Outputs a CSV file containing evaluated postfix expressions
+     * 
+     * @param collectList The list of evaluated postfix expressions
+     * @throws IOException
+     */
     public static void outputCSVFile(List<String> collectlist) throws IOException {
         FileWriter csvWriter = new FileWriter("OutputCSVFile.csv");
         for (String element : collectlist) {
-            csvWriter.write(element);
-            csvWriter.write(", ");
+            csvWriter.write(element + ",");
         }
         csvWriter.flush();
         csvWriter.close();
--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-#Postfix expression evaluator for CSV files
+# Postfix expression evaluator for CSV files
 
-##To run:
+## To run:
 
 1. Clone this repository.
 
 2. Use the following command at the commandline to enter the directory named "evaluator" and build the java file:
 
-- \$ cd evaluator && find . -name ‘\*.java’ | xargs javac
+\$ cd evaluator && find . -name ‘\*.java’ | xargs javac
 
-3. Run the PostFix class on the default csv file provided:
+3. Run the PostFix class on the default csv file provided (TestInputCSVFile.csv):
 
-- \$ java PostFix
+\$ java PostFix
 
 4. Or, run the PostFix class on your own csv file:
 
-- \$ java PostFix YourInputCSVFile.csv
+\$ java PostFix YourInputCSVFile.csv
 
 5. Check the directory for an output csv file named OutputCSVFile.csv:
 
-- \$ test -f OutputCSVFile.csv && echo "File found"
+\$ test -f OutputCSVFile.csv && echo "File found"
